cf580af8e7aa97ccb0ccaaa57368f1b013d61ddd
BOILLOD Lucien <lucienboillod@gmail.com>
2018-04-19 15:36:12 +0200

    Update recipe

--- conanfile.py
+++ conanfile.py
@@ -18,6 +18,7 @@ class TulipConan(ConanFile):
     source_dir = "tulip"
     export_sources = ["CmakeLists.txt"]
     generators = "cmake"
+    short_paths = True
 
     def requirements(self):
         self.requires("zlib/1.2.11@conan/stable")
@@ -26,6 +27,7 @@ class TulipConan(ConanFile):
         self.requires("gtest/1.8.0@bincrafters/stable")
         self.requires("freetype/2.9.0@lboillod/testing")
         self.requires("glew/2.1.0@dimi309/stable")
+        self.requires("Qt/5.9.0@lboillod/testing")
 
     def source(self):
         cloned_sources = os.path.join(self.source_folder, self.source_dir)
@@ -33,9 +35,9 @@ class TulipConan(ConanFile):
         with tools.chdir(cloned_sources):
             self.run("git checkout {commit}".format(commit=self.version))
         os.rename(cloned_sources, "tulip")
-#         tools.replace_in_file("tulip/CMakeLists.txt", "PROJECT(tulip)", """PROJECT(tulip)
-# include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-# conan_basic_setup()""")
+        tools.replace_in_file("tulip/CMakeLists.txt", "PROJECT(tulip)", """PROJECT(tulip)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()""")
 
 
     def configure(self):
@@ -51,9 +53,7 @@ class TulipConan(ConanFile):
             cmake.definitions["TULIP_BUILD_DOC"] = "OFF"
             cmake.definitions["TULIP_USE_QT5"] = "ON"
             cmake.definitions["TULIP_FIXUP_BUNDLE"] = "ON"
-            # Put Qt path here
-            cmake.definitions["CMAKE_PREFIX_PATH"] = "C:/Qt/5.10.1/msvc2015_64/lib/cmake"
-            cmake.configure()
+            cmake.configure(source_dir=os.path.join(self.source_folder, self.source_dir))
             cmake.build()
             cmake.install()
 
